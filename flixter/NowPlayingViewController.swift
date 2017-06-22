17cecc0846f8856bf5bcb4d9e305d69517d8ff35
Xiuya Yao <xiuyayao@xiuyayao.dhcp.thefacebook.com>
2017-06-22 16:58:00 -0700

    Added posters

--- flixter/NowPlayingViewController.swift
+++ flixter/NowPlayingViewController.swift
@@ -74,6 +74,12 @@ class NowPlayingViewController: UIViewController, UITableViewDataSource {
         cell.titleLabel.text = title
         cell.overviewLabel.text = overview
         
+        let posterPathString = movie["poster_path"] as! String
+        let baseURLString = "https://image.tmdb.org/t/p/w500"
+        
+        let posterURL = URL(string: baseURLString + posterPathString)!
+        cell.powerImageView.af_setImage(withURL: posterURL)
+        
         return cell
     }
 
