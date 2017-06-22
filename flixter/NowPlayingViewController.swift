71eb5217e6e77b8545be46c6531d87c9c1aa8522
Xiuya Yao <xiuyayao@xiuyayao.dhcp.thefacebook.com>
2017-06-21 17:20:32 -0700

    Table View Setup

---
+++ flixter/NowPlayingViewController.swift
@@ -0,0 +1,79 @@
+//
+//  NowPlayingViewController.swift
+//  flixter
+//
+//  Created by Xiuya Yao on 6/21/17.
+//  Copyright © 2017 Xiuya Yao. All rights reserved.
+//
+
+import UIKit
+
+class NowPlayingViewController: UIViewController, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.dataSource = self
+        
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=b0dc2f9b663ecf33aed9b648055a6c7f")!
+        // Above statement returns an optional url, so bang (!) added at end to force unwrapping
+        
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        // reloadIgnoringLocalCacheData means our network request is always going to pukll from network even if data is in cache
+        // 10 is time in seconds
+        
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        // When our network request returns, it'll jump back on our main
+        
+        let task = session.dataTask(with: request) { (data, response, error) in
+            // This code below will run with network request returns
+            if let error = error {
+                print(error.localizedDescription)
+            } else if let data = data {
+                // got JSON data back and now parse it
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                // try! force unwraps it
+                // will crash is there's an error
+                // cast into Swift dictionary
+                // print(dataDictionary)
+                let movies = dataDictionary["results"] as! [[String: Any]]
+                for movie in movies {
+                    let title = movie["title"] as! String
+                    print(title)
+                }
+            }
+        }
+        task.resume()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath)
+        return cell
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
